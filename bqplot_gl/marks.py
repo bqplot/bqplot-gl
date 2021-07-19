93d8bfe026bfef23f55530edf05374e32107c5dc
martinRenou <martin.renou@gmail.com>
2021-07-19 11:43:21 +0200

    Fix module name

--- bqplot_gl/marks.py
+++ bqplot_gl/marks.py
@@ -6,3 +6,5 @@ from bqplot import Scatter
 class ScatterGL(Scatter):
     _view_name = Unicode('ScatterGLView').tag(sync=True)
     _model_name = Unicode('ScatterGLModel').tag(sync=True)
+    _model_module = Unicode('bqplot-gl').tag(sync=True)
+    _view_module = Unicode('bqplot-gl').tag(sync=True)
