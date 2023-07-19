e4d375e39635151a5c0bd5267890b639ab285fe3
martinRenou <martin.renou@gmail.com>
2023-07-19 09:43:30 +0200

    Migrate to hatch

--- bqplot_gl/_version.py
+++ bqplot_gl/_version.py
@@ -4,5 +4,4 @@
 # Copyright (c) The bqplot Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 0)
-__version__ = ".".join(map(str, version_info))
+__version__ = "0.1.0"
