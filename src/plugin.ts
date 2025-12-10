d8951115a53212beef34fe48f921715bed2b66fd
martinRenou <martin.renou@gmail.com>
2025-12-10 10:36:40 +0100

    Scope plugin + update snapshots (#22)
    * Scope plugin
    
    * Update Playwright Snapshots
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/plugin.ts
+++ src/plugin.ts
@@ -11,7 +11,7 @@ import * as widgetExports from './index';
 
 import { MODULE_NAME, MODULE_VERSION } from './version';
 
-const EXTENSION_ID = 'bqplot-gl:plugin';
+const EXTENSION_ID = `@bqplot/bqplot-gl:plugin${MODULE_VERSION}`;
 
 /**
  * The bqlotGL plugin.
